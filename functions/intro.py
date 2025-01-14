e80c0b3ac2954e2a254ac6886e8e9888e7fbd80f
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-14 16:59:12 +0100

    <feat> Add intro function
    With this function we print the instructions so that the user knows how to use the symbols correctly

---
+++ functions/intro.py
@@ -0,0 +1,8 @@
+def intro():
+    print('''Welcome to the calculator.
+Please note the following symbology when performing your operations:
+** Exponentiation
+* Multiplication
+/ Division
++ Addition
+- Subtraction''')
