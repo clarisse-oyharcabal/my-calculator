1fd335887339d7adce8b5afe53095b4fffbb2d6a
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-15 15:20:43 +0100

    <feat> adds the parse() function to clean up and separate the received data
    we receive the argument operation by parameter, we eliminate the possible blanks with .replace(), we create the variables for each number and the operator, in a loop we distribute the information and then we convert the numbers into floats, returning by Return le number_1, operator, and the number_2

---
+++ functions/parse.py
@@ -0,0 +1,29 @@
+def parse(operation):
+    operation = operation.replace(" ", "")
+    
+    number_1 = ""
+    operator = ""
+    number_2 = ""
+    operator_found = False
+    
+    for char in operation:
+        if char in "+-*/e" and not operator_found:
+            operator = char
+            operator_found = True
+        elif not operator_found:
+            number_1 += char
+        else:
+            number_2 += char
+    
+    number_1 = float(number_1)
+    number_2 = float(number_2)
+    
+    return number_1, operator, number_2
+
+
+operation = "365e 5"
+number_1, operator, number_2 = parse(operation)
+
+print("number_1:", number_1)
+print("operator:", operator)
+print("number_2:", number_2)
