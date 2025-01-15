73ed17b0b06375bcda8329b2ae0d25b506ce7580
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-15 13:57:12 +0100

    <feat> import subtraction() function to main.py file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functions.exponentiation import exponentiation
 from functions.multiplication import multiplication
 from functions.division import division
 from functions.addition import addition
+from functions.subtraction import subtraction
 
 def main():
     intro()
