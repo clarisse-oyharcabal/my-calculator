1d73d567f0806b3180fbffdd75213e290d1eba73
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-15 13:02:41 +0100

    <feat> add division() function code
    with the division() function we will receive two digits by parameter, verify if the second number is not a zero (0) and perform the division operation returning the result by Return
    
    A list is sent by Return with the elements of the operation and the result at the end of the list or an string in the zero (0) case

---
+++ functions/division.py
@@ -0,0 +1,9 @@
+def division(number_1, number_2):
+    if number_2 == 0:
+        operation = "Zero is not a valid value in a division."
+    else:
+        result = number_1 / number_2
+        operation = [number_1, '/', number_2, result]
+    return operation
+
+print(division(0,10))
