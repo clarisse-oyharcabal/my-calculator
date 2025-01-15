e6a14cb9958756c55d58e0d0b0f3c97f9c098fe0
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-15 13:48:56 +0100

    <feat> import addition() function to main.py file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from functions.intro import intro
 from functions.exponentiation import exponentiation
 from functions.multiplication import multiplication
+from functions.division import division
 
 def main():
     intro()
