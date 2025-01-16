5680ba680f09fe0661d9db237a1fba7232abdae4
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-16 13:43:57 +0100

    <refact> add r Square root convention to intro() function text

--- functions/intro.py
+++ functions/intro.py
@@ -5,6 +5,7 @@ Please note the following symbology when performing your operations:
 -------------------------------------------------------------------
 
 e Exponentiation
+r Square root
 * Multiplication
 / Division
 + Addition
