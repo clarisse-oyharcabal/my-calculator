646c8a1b25f1d943b46e340c8b08707da32d6a4d
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-16 14:06:28 +0100

    <refactor> change Boolean valid_sqrt_operator name to valid_sqrt_operation

--- functions/input_validator.py
+++ functions/input_validator.py
@@ -19,15 +19,14 @@ def input_validator(number_1, operator, number_2):
         else:
             break
 
-    valid_sqrt_operator = True
-    while operator == "r":
+    valid_sqrt_operation = True
+    if operator == "r":
         if number_1 < 0:
             print("Square root operation is valid only for positive numbers.")
-            valid_sqrt_operator = False
-            return valid_sqrt_operator
-        #sqrt_return = number_1, operator, None, valid_sqrt_operator # Retourner le nombre si pas d'erreur
-        #return sqrt_return
-        return number_1, operator, None, valid_sqrt_operator
+            valid_sqrt_operation = False
+            return valid_sqrt_operation
+        print("We are going to remove the second number, as we can only perform the square root operation that way, sorry.")
+        return number_1, operator, None, valid_sqrt_operation
 
     
     valid_number_2 = True
@@ -48,21 +47,21 @@ def input_validator(number_1, operator, number_2):
             valid_number_2 = False
             return valid_number_2
     
-    return number_1, operator, number_2, valid_number_1, valid_operator, valid_sqrt_operator, valid_division_operation, valid_number_2
+    return number_1, operator, number_2, valid_number_1, valid_operator, valid_sqrt_operation, valid_division_operation, valid_number_2
 
 
 
 ########
 
-number_1 = "23"
-operator = "+"
-number_2 = "4"
+"""number_1 = "23"
+operator = "/"
+number_2 = "0"
 print(number_1, operator, number_2)
-print(input_validator(number_1, operator, number_2))
+print(input_validator(number_1, operator, number_2))"""
 
-number_1, operator, number_2, valid_number_1, valid_operator, valid_sqrt_operator, valid_division_operation, valid_number_2 = input_validator(number_1, operator, number_2)
+"""number_1, operator, number_2, valid_number_1, valid_operator, valid_sqrt_operator, valid_division_operation, valid_number_2 = input_validator(number_1, operator, number_2)
 print(f"valid_number_1: {valid_number_1}")
 print(f"valid_operator: {valid_operator}")
 print(f"valid_sqrt_operator: {valid_sqrt_operator}")
 print(f"valid_division_operation: {valid_division_operation}")
-print(f"valid_number_2: {valid_number_2}")
+print(f"valid_number_2: {valid_number_2}")"""
