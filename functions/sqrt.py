8a4a691062fad225c1ef66c7ead12c619f0a4c5f
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-18 20:16:01 +0100

    <debug> number_2 None case
    include one and two operands in sqrt operation

--- functions/sqrt.py
+++ functions/sqrt.py
@@ -1,13 +1,11 @@
-def sqrt(number_1, number_2):
+def sqrt(number_1, number_2=None):
     if number_2 is None:
         result = number_1 ** 0.5
-        operation = ["sqrt de ", number_1, result]
-    elif isinstance(number_2, float):
-        result = (number_1 ** 0.5) * number_2
-        operation = ["sqrt de ", number_1, '*', number_2, result]
-    return operation
-
-"""number_1 = 16.0
-number_2 = None
-print(sqrt(number_1, number_2))
-"""
+        return (number_1, "r", None, result)
+    else:
+        try:
+            number_2 = float(number_2)
+            result = (number_1 ** 0.5) * number_2
+            return (number_1, "r", number_2, result)
+        except ValueError:
+            return "Invalid operation (detected in sqrt.py)"
