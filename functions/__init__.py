ce4768f6fb26be7c7de78414903c5ce6082654c1
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-18 20:21:09 +0100

    <feat> add __init__.py file to define functions folder as modules pack
    this feat optimaze the program and resolve a bug related with functions importstions to main file

---
+++ functions/__init__.py
@@ -0,0 +1,13 @@
+# __init__.py
+from .addition import addition
+from .division import division
+from .multiplication import multiplication
+from .subtraction import subtraction
+from .sqrt import sqrt
+from .exponentiation import exponentiation
+from .intro import intro
+from .input_operation import input_operation
+from .input_validator import input_validator
+from .parse import parse
+from .calculate import calculate
+from .print_results import print_results
