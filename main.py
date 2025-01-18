3a7f66b785556a402c2d3144687570b0ec8a52ed
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-18 20:31:25 +0100

    <debug> resolve infinite while
    validated factors are processed in each loop

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from functions.intro import intro
-from functions.input_operation import input_operation
-from functions.parse import parse
-from functions.input_validator import input_validator
-from functions.sqrt import sqrt
-from functions.exponentiation import exponentiation
-from functions.multiplication import multiplication
-from functions.division import division
-from functions.addition import addition
-from functions.subtraction import subtraction
-from functions.calculate import calculate
+from functions import intro, input_operation, input_validator, parse, calculate, print_results
 
 
 def main():
@@ -17,43 +7,16 @@ def main():
     while True:
         operation = input_operation()
         number_1, operator, number_2 = parse(operation)
+        
         validated = input_validator(number_1, operator, number_2)
-
-
-        number_1 = validated[0]
-        operator = validated[1]
-        number_2 = validated[2]
-        valid_number_1 = validated[3]
-        valid_operator = validated[4]
-        valid_sqrt_operation = validated[5]
-        valid_division_operation = validated[6]
-        valid_number_2 = validated[7]
-    
-    
-        while not valid_number_1:
-            operation = input_operation()
-            number_1, operator, number_2 = parse(operation)
-            validated = input_validator(number_1, operator, number_2)
-        while not valid_number_2:
-            operation = input_operation()
-            number_1, operator, number_2 = parse(operation)
-            validated = input_validator(number_1, operator, number_2)
-        while not valid_operator:
-            operation = input_operation()
-            number_1, operator, number_2 = parse(operation)
-            validated = input_validator(number_1, operator, number_2)
-        while not valid_sqrt_operation:
-            operation = input_operation()
-            number_1, operator, number_2 = parse(operation)
-            validated = input_validator(number_1, operator, number_2)
-        while not valid_division_operation:
-            operation = input_operation()
-            number_1, operator, number_2 = parse(operation)
-            validated = input_validator(number_1, operator, number_2)
+        
+        number_1, operator, number_2 = validated[:3]
+        valid_number_1, valid_operator, valid_sqrt_operation, valid_division_operation, valid_number_2 = validated[3:]
+        
+        if valid_number_1 and valid_number_2 and valid_operator and valid_sqrt_operation and valid_division_operation:
+            result = calculate((number_1, operator, number_2))
+            print_results((result))
         else:
-            operation = number_1, operator, number_2
-        return operation
-    #
-        print(calculate(operation))
-    
+            print("Please, try again.")
+
 main()
