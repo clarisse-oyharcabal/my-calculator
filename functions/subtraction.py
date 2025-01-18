fcc533859557dd63d27f8b8300a3a00b2f1d3342
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-18 20:28:35 +0100

    <refactor> changes quotation marks from simple to double

--- functions/subtraction.py
+++ functions/subtraction.py
@@ -1,4 +1,4 @@
 def subtraction(number_1, number_2):
     result = number_1 - number_2
-    operation = [number_1, '-', number_2, result]
+    operation = [number_1, "-", number_2, result]
     return operation
