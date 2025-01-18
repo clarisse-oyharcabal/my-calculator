208c3b733b2b991e38e57f14424c30bda4346ede
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-18 20:23:49 +0100

    <refactor> changes quotation marks from simple to d ouble

--- functions/division.py
+++ functions/division.py
@@ -3,5 +3,5 @@ def division(number_1, number_2):
         operation = "Zero is not a valid value in a division."
     else:
         result = number_1 / number_2
-        operation = [number_1, '/', number_2, result]
+        operation = [number_1, "/", number_2, result]
     return operation
