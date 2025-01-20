c29b8f6c6193e60b806bdf6667937ba97c847196
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-20 11:52:48 +0100

    <feat> add prueba.py file to explorer the one line expression input capability

---
+++ prueba.py
@@ -0,0 +1,16 @@
+from functions import custom_calculator, menu
+
+def main():
+    while True:
+        expression = input("Enter an expression: ")
+        try:
+            result = custom_calculator(expression)
+            print(f"Result: {result}")
+        except ValueError as e:
+            print(e)
+        again = input("Would you like to perform another calculation? (yes/no): ").strip().lower()
+        if again != "yes":
+            break
+
+if __name__ == "__main__":
+    main()
