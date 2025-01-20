a92d0c7bd709e72ab8c936e47e2cee6e865d3b40
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-20 11:58:41 +0100

    <feat> add custom_calculator and menu functions to package

--- functions/__init__.py
+++ functions/__init__.py
@@ -11,3 +11,7 @@ from .input_validator import input_validator
 from .parse import parse
 from .calculate import calculate
 from .print_results import print_results
+from .history import save_to_history, load_history, view_history, clear_history
+from .menu import menu 
+from .perform_calculation import perform_calculation
+from .custom_calculator import custom_calculator
